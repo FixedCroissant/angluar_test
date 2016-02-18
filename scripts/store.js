3a49c34ffeb51115bdf588556406a86f2499f525
Joshua Williams <jjwill10@ncsu.edu>
2016-02-18 11:38:54 -0500

    Using logic within a new controller that is called PanelController that handles the show/hide of the div sections. Added bootstrap css for this example to show correctly. Also uses thumbnail(s) for imagesof the product.

--- scripts/store.js
+++ scripts/store.js
@@ -4,19 +4,35 @@ var app=angular.module('store',[]);
 
 //New controller that is attached to the store module.
 app.controller('StoreController',function(){
-    //add inside the function.
-    this.products = gems;
+        //add inside the function.
+        this.products = gems;
     });
+
+//New panel controller
+    app.controller("PanelController",function(){
+        this.tab=1;
+
+        this.selectTab = function(setTab){
+            this.tab=setTab;
+        };
+
+        this.isSelected = function(checkTab){
+            return this.tab===checkTab;
+        };
+
+    });
+
 
     //what's a GEM(s)? list them here.
     var gems = [{
         name: 'Dodecahedron',
         price: 2.95,
         description:'A rare jewel that is worth far more than this initial asking price.',
+        review:'I love this item',
         images:[
             {
                 full: '../images/dodecahedron-01-full.jpg',
-                thumb:'dodecahedron-01-thumb.jpg'
+                thumb:'../images/dodecahedron-01-thumb.jpg'
             }
         ],
 
@@ -32,3 +48,5 @@ app.controller('StoreController',function(){
         }
         ]/*Close gems array*/
 })();
+
+
