e4a5fc2f262efde4c58ce8dd5787b584493468ed
Joshua Williams <jjwill10@ncsu.edu>
2016-02-18 10:29:14 -0500

    Using the ng-src to create images on the fly. Uisng example from shaping up with angular.js level/2

--- scripts/store.js
+++ scripts/store.js
@@ -13,6 +13,13 @@ app.controller('StoreController',function(){
         name: 'Dodecahedron',
         price: 2.95,
         description:'A rare jewel that is worth far more than this initial asking price.',
+        images:[
+            {
+                full: '../images/dodecahedron-01-full.jpg',
+                thumb:'dodecahedron-01-thumb.jpg'
+            }
+        ],
+
         canPurchase:true,
         soldOut:true
         },
